740e4667be2f5875efbca33bf6ddffab5f71fe36
gary804 <gary.guolin.jin@gmail.com>
2015-08-13 09:13:05 -0700

    update ArrayOperation.java

--- ArrayOperation.java
+++ ArrayOperation.java
@@ -53,6 +53,12 @@ public class ArrayOperation{
 		}
 	}
 
+	public static void arrayUpdateTest(int[] array){
+//array variable is the reference of original array, not the copy of the array,
+// so its change here can be seen outside the function
+		array[0]= 100;
+	}
+
 	public static void main(String[] args) {
 		int[] a={0,1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 		int[] b={1,2,3,4,8,5,6,7,8,9,0};
@@ -66,5 +72,7 @@ public class ArrayOperation{
 		for (int i=-1; i<a.length; i++) {
 			System.out.println("Looking for "+i+"; its index is: "+binarySearch(a, i));
 		}
+		arrayUpdateTest(b);
+		System.out.println(b[0]);	//print 100, so the change in arrayUpdateTest can be seen here
 	}
 }
