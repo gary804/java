13a788c6c8ca209e55577bdc37abf5e4fd023157
gary804 <gary.guolin.jin@gmail.com>
2015-08-14 16:14:56 -0700

    update ArrayOperation.java for add revers_words(String)

--- ArrayOperation.java
+++ ArrayOperation.java
@@ -1,3 +1,6 @@
+
+import java.util.StringTokenizer;
+
 public class ArrayOperation{
 	public static boolean hasDuplicate(int[] array) {
 		for (int i = 0; i < array.length; i++) {
@@ -76,6 +79,63 @@ public class ArrayOperation{
 		array[0]= 100;
 	}
 
+	public static String reverse_words(String str){
+		StringTokenizer st = new StringTokenizer(str);
+		StringBuilder sb = new StringBuilder();
+		while(st.hasMoreTokens()){
+			sb.insert(0, st.nextToken()+" ");
+		}
+		sb.trimToSize();
+		if (sb.length()-1 >=0) sb.setLength(sb.length()-1);
+		return sb.toString();
+	}
+
+	public static String reverse_words1(String str){
+		String[] sa = str.split("\\s");	//at least one empty string in sa
+		StringBuilder sb = new StringBuilder();
+		for (int i=0; i< sa.length; i++){
+			sb.insert(0, sa[i]+" ");
+		}
+		sb.trimToSize();
+		sb.setLength(sb.length()-1);
+		return sb.toString();
+	}
+
+	public static String reverse_words2(String str){
+		String[] sa = str.split("\\s");
+		StringBuilder sb = new StringBuilder(sa[sa.length-1]);
+		for (int i=sa.length-2; i>=0; i--){
+			sb.append(" "+sa[i]);
+		}
+		sb.trimToSize();
+		return sb.toString();
+	}
+
+	public static String reverse_words3(String str){
+		String[] sa = str.split("\\s");
+		String st = sa[sa.length-1];
+		for (int i=sa.length-2; i>=0; i--){
+			st+=" "+sa[i];
+		}
+		return st;
+	}
+
+	public static String reverse_characters(String str){
+		String[] sa = str.split("");	//split characters
+		String st = sa[sa.length-1];
+		for (int i=sa.length-2; i>=0; i--){
+			st+=sa[i];
+		}
+		return st;
+	}
+	public static String reverse_characters1(String str){
+		String st = "";
+		for (int i=str.length()-1; i>=0; i--){
+			st+=str.charAt(i);
+		}
+		return st;
+	}
+
 	public static void main(String[] args) {
 		int[] a={0,1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 		int[] b={1,2,3,4,8,5,6,7,8,9,0};
@@ -86,16 +146,32 @@ public class ArrayOperation{
 */
 		System.out.println((""+hasDuplicate(a)).toUpperCase());
 		System.out.println((""+hasDuplicate(b)).toUpperCase());
+		
 		for (int i=-1; i<a.length; i++) {
 			System.out.println("Looking for "+i+"; its index is: "+binarySearch(a, i));
 		}
+
 		arrayUpdateTest(b);
 		System.out.println(b[0]);	//print 100, so the change in arrayUpdateTest can be seen here
+
 assciToInteger:	{
 			String str = "+1234";
 			String str1 = "-1234.5";
 			String str2 = "-123a4.5";
 			System.out.print(""+atoi(str)+", "+atoi(str1)+", "+atoi(str2)+"\n");
 		}
+reverseStringTest: {
+		String str = "Write a function to reverse the order of words in a string in place.";
+		System.out.println("'"+reverse_words(str)+"'");
+		System.out.println("'"+reverse_words1(str)+"'");
+		System.out.println("'"+reverse_words2(str)+"'");
+		System.out.println("'"+reverse_words3(str)+"'");
+		System.out.println("'"+reverse_words("")+"'");
+		System.out.println("'"+reverse_words1("")+"'");
+		System.out.println("'"+reverse_words2("")+"'");
+		System.out.println("'"+reverse_words3("")+"'");
+		System.out.println("'"+reverse_characters(str)+"'");
+		System.out.println("'"+reverse_characters1(str)+"'");
 	}
+}
 }
